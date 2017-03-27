58c857fc65df779e92845195e11e1f4330a8d8b7
pololamenace <paulchabert6@gmail.com>
2017-03-27 14:24:42 +0200

    lru.c works: init, add, remove, mtu (move a page to the head),print

--- bf/bf.c
+++ bf/bf.c
@@ -3,11 +3,16 @@
 #include "minirel.h"
 
 
-/*
-*init the BF layer : 
+/*****************************************************************************/
+/*init the BF layer : 
 ***create the buffer entries (empty at the beginning ) 
 ***add them to the free list
-***init the hashtable
-*/
+***init the hashtable                                                        */
+/*****************************************************************************/
+
+
 void BF_Init(void){
 }
+
+
+
