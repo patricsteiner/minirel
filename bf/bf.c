4f1b0b64c3a716ec0ebcade1dbfc93a8945f3ce7
ajubin <antoine.jubin@depinfonancy.net>
2017-04-02 16:25:56 +0900

    add of BF_GetBuf and BF_Init

--- bf/bf.c
+++ bf/bf.c
@@ -68,53 +68,69 @@ int BF_AllocBuf(BFreq bq, PFpage **fpage){
 int BF_GetBuf(BFreq bq, PFpage **fpage){
 	BFhash_entry* h_entry;
 	BFpage* bfpage_entry;
+	BFpage* victim;
+	int res;
 
 	h_entry = ht_get(ht, bq->fd, bq->pagenum);
 
+	//page already in buffer
 	if(h_entry != NULL){
+		//to check
 		h_entry->count += 1;
-	}else{
-		//get a empty freelist BFpage, add it to the hashtable and ti the lru list		
-		bfpage_entry = fl_give_one(fl);
-		if(bfpage_entry != NULL){
-			//first add in the lru and then in the hashtable
-			if(lru_add(lru, bfpage_entry) == 0 && ht_add(ht, bfpage_entry) == 0){
-			}else{
-				//error handling : impossible to add the entry
-				// error code add into pf.h (-9)
-				return PFE_GETBUF;
-			}
-
-			
-		}else{
-			//remove a page from the lru list
-			//add it to the freelist (the page will be cleared)
-			//remove from the hashtable
-			//add a page to the lru list (with the right information) with fl_give_one
-			//notify the hash table from this change
-			
-
-		}	
+		&fpage = h_entry->fpage;
+		return BFE_OK;
 	}
 
-	return 0;
-	// a memory block is a BFpage
-	// The bufferRequest is composed by (fd, unixfd, pagenum, dirty) 
-	/*	ALGO
-	if fd in LRU List (check with the hashtable)
-		pincount += 1
-	else
-		allocate a new buffer page (by page replacement if no free in the freelist)
-		TODO : add a function in freelist.c freeSpaceAvailable(Freelist* fl)
-	 	read the file page into fpage.bufferpage
-	 	pincount set to 1
-	 	dirty set to FALSE
-		other fields
-	the page then become the most recently used
-	return BFE_OK if correct
+
+	//page not in buffer
+	else{
+		bfpage_entry = fl_give_one(fl);
+
+		//No more  place in the buffer <=> No more freespace
+		if( bfpage_entry == NULL){
+			
+			//find a victim
+			res = lru_remove(lru, &victim);
+			
+			if(lru_remove(lru, &victim) != 0){return res;} 	//no victim found
+			else											//victim found
+			{
+				if(victim->dirty == TRUE)					//victim dirty : try to flush it, trhow error otherwise
+				{
+					if(pwrite(victim->unixfd, victim->fpage->pagebuf, PAGE_SIZE, ((victim->pagenum)-1)*PAGE_SIZE) != PAGE_SIZE){
+						return BFE_INCOMPLETEWRITE;
+					}
+				}
+				//remove victim
+				ht_remove(ht, victim);
+				fl_add(fl, victim);
+			}
+
+			bfpage_entry = fl_give_one(fl);
+		}
+		
+		//space available in buffer(add page to LRU and HT)
+		if(lru_add(lru, bfpage_entry) == BFE_OK && ht_add(ht, bfpage_entry) == BFE_OK){
+		}else{return BFE_PAGENOTOBUF;}
 
 
-	 */
+		//try to read the file asked
+		if(fread(bq->unixfd, bfpage_entry->fpage->pagebuf, PAGE_SIZE, ((bq->pagenum)-1)*PAGE_SIZE) == -1){
+			return BFE_INCOMPLETEREAD;
+		}
+
+		//set the correct parameters
+		bfpage_entry->count = 1;
+		bfpage_entry->dirty = FALSE;
+		bfpage_entry->fd = bq->fd;
+		bfpage_entry->pagenum = bq->pagenum;
+		bfpage_entry->unixfd = bq->unixfd;
+
+		//value returned to the user
+		&fpage = bfpage_entry->fpage;
+		return BFE_OK;
+	
+	}
 }
 
 
